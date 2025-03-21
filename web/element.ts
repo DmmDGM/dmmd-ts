e6c9605a534480e193c2706bd2cfcaa65db22425
DmmD GM <Dm12332131mDGaming@gmail.com>
2025-03-21 18:15:19 +0000

    save

--- web/element.ts
+++ web/element.ts
@@ -36,89 +36,159 @@ export const elementDefaultOptions: ElementSimplifiedOptions = {
     style: {},
     text: ""
 };
+/** Collection of element modifiers. */
+export const modifiers = {
+    attributes: modifyAttributes,
+    classes: modifyClasses,
+    events: modifyEvents,
+    href: modifyHref,
+    html: modifyHtml,
+    id: modifyId,
+    parent: modifyParent,
+    src: modifySrc,
+    style: modifyStyle,
+    text: modifyText
+};
 
-/** Modifies element with a simplied set of options. */
+/** Modifies an element with a simplied set of options. */
 export function modify<
     Element extends HTMLElement = HTMLElement
 >(
     element: Element,
     options: Partial<ElementSimplifiedOptions<Element>>
-): Element {
+): void {
     // Modifies element
     const optionNames = Object.getOwnPropertyNames(options);
     for(let i = 0; i < optionNames.length; i++) {
         const optionName = optionNames[i] as keyof typeof options;
-        const option = options[optionName] ?? elementDefaultOptions[optionName];
-        switch(optionName) {
-            case "attributes": {
-                const attributes = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                const attributeNames = Object.getOwnPropertyNames(attributes);
-                for(let j = 0; j < attributeNames.length; j++) {
-                    const attributeName = attributeNames[j] as keyof typeof attributes;
-                    const attribute = attributes[attributeName];
-                    element.setAttribute(attributeName, attribute);
-                }
-                break;
-            }
-            case "classes": {
-                const classes = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                for(let i = 0; i < classes.length; i++) {
-                    const token = classes[i];
-                    element.classList.add(token);
-                }
-                break;
-            }
-            case "events": {
-                const events = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                const eventNames = Object.getOwnPropertyNames(events);
-                for(let j = 0; j < eventNames.length; j++) {
-                    const eventName = eventNames[j] as keyof typeof events;
-                    const event = events[eventName];
-                    element.addEventListener(eventName, event as EventListenerOrEventListenerObject);
-                }
-                break;
-            }
-            case "href": {
-                const href = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                element.setAttribute("href", href);
-                break;
-            }
-            case "html": {
-                const html = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                element.innerHTML = html;
-                break;
-            }
-            case "id": {
-                const id = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                element.id = id;
-                break;
-            }
-            case "parent": {
-                const parent = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                parent.appendChild(element);
-                break;
-            }
-            case "src": {
-                const src = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                element.setAttribute("src", src);
-                break;
-            }
-            case "style": {
-                const style = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                Object.assign(element.style, style);
-                break;
-            }
-            case "text": {
-                const text = option as ElementSimplifiedOptions<Element>[typeof optionName];
-                element.innerText = text;
-                break;
-            }
-        }
+        if(optionName in modifiers) modifiers[optionName](element, )
     }
-
-    // Returns element
-    return element;
 }
+
+/** Modifies an element's attributes */
+export function modifyAttributes<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    attributes: ElementSimplifiedOptions["attributes"]
+): void {
+    // Modifies attributes
+    const attributeNames = Object.getOwnPropertyNames(attributes);
+    for(let j = 0; j < attributeNames.length; j++) {
+        const attributeName = attributeNames[j] as keyof typeof attributes;
+        const attribute = attributes[attributeName];
+        element.setAttribute(attributeName, attribute);
+    }
+}
+
+/** Modifies an element's class list. */
+export function modifyClasses<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    classes: ElementSimplifiedOptions["classes"]
+): void {
+    // Modifies classes
+    for(let i = 0; i < classes.length; i++) {
+        const token = classes[i];
+        element.classList.add(token);
+    }
+}
+
+/** Modifies an element's events. */
+export function modifyEvents<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    events: ElementSimplifiedOptions["events"]
+): void {
+    // Modifies events
+    const eventNames = Object.getOwnPropertyNames(events);
+    for(let j = 0; j < eventNames.length; j++) {
+        const eventName = eventNames[j] as keyof typeof events;
+        const event = events[eventName] as EventListenerOrEventListenerObject;
+        element.addEventListener(eventName, event);
+    }
+}
+
+/** Modifies an element's href. */
+export function modifyHref<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    href: ElementSimplifiedOptions["href"]
+): void {
+    // Modifies events
+    element.setAttribute("href", href);
+}
+
+/** Modifies an element's inner html. */
+export function modifyHtml<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    html: ElementSimplifiedOptions["html"]
+): void {
+    // Modifies events
+    element.innerHTML = html;
+}
+
+/** Modifies an element's id. */
+export function modifyId<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    id: ElementSimplifiedOptions["id"]
+): void {
+    // Modifies events
+    element.id = id;
+}
+
+/** Modifies an element's parent. */
+export function modifyParent<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    parent: ElementSimplifiedOptions["parent"]
+): void {
+    // Modifies events
+    parent.appendChild(element);
+}
+
+/** Modifies an element's src. */
+export function modifySrc<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    src: ElementSimplifiedOptions["src"]
+): void {
+    // Modifies events
+    element.setAttribute("src", src);
+}
+
+/** Modifies an element's style. */
+export function modifyStyle<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    style: ElementSimplifiedOptions["style"]
+): void {
+    // Modifies events
+    Object.assign(element.style, style);
+}
+
+/** Modifies an element's inner text. */
+export function modifyText<
+    Element extends HTMLElement = HTMLElement
+>(
+    element: Element,
+    text: ElementSimplifiedOptions["text"]
+): void {
+    // Modifies events
+    element.innerText = text;
+}
+
+
 
 /** Creates, initializes, and returns an element with a simplified set of options. */
 export function create<
