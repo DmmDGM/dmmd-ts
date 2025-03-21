1bb72b68206dd427ae6fdf636bfacfd70f636596
DmmD GM <Dm12332131mDGaming@gmail.com>
2025-03-21 13:44:46 +0000

    push

--- web/element.ts
+++ web/element.ts
@@ -1,7 +1,25 @@
-export function create<Tag extends keyof HTMLElementTagNameMap>(tag: Tag): HTMLElementTagNameMap[Tag] {
-    const element = document.createElement(tag);
+type ElementBaseOptions<
+    TagName extends keyof HTMLElementTagNameMap,
+    Tag = HTMLElementTagNameMap[TagName]
+> = {
+    class: string[];
+    events: Partial<{ [Event in keyof HTMLElementEventMap]: (
+        this: Tag,
+        event: HTMLElementEventMap[Event]
+    ) => any }>;
+    html: string;
+    id: string;
+    parent: HTMLElement;
+    style: CSSStyleDeclaration
+    text: string;
+} & { [ Key in keyof Tag ]: Tag[Key] };
 
+/** Creates, initializes, and returns an element with predefined options. */
+export function create(
+    // tag: Tag,
+    // options: Partial<object> = {}
+): HTMLElementTagNameMap[Tag] {
+    const element = document.createElement(tag);
+    element.style
     return element;
 }
-
-create("a")
