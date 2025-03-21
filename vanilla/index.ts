63f6809c984dbfae40d17db454f52c1df804294f
DmmD GM <dm12332131mdgaming@gmail.com>
2025-03-20 23:54:58 -0400

    math

--- vanilla/index.ts
+++ vanilla/index.ts
@@ -1,7 +1,9 @@
 // Imports
+import * as math from "./math";
 import * as prototype from "./prototype";
 import * as time from "./time";
 
 // Exports
+export { math };
 export { prototype };
 export { time };
---
+++ vanilla/math.ts
@@ -0,0 +1,5 @@
+/** Clamps and returns a value between a minimum and a maximum value */
+export function clamp(value: number, minimum: number, maximum: number): number {
+    // Returns value
+    return Math.min(Math.max(value, minimum, maximum));
+}
