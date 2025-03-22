f54cda25ea7c8e7bca5125d6e3f4e26e55b5c3dd
DmmD GM <dm12332131mdgaming@gmail.com>
2025-03-22 19:27:52 -0400

    push

--- web/element.ts
+++ web/element.ts
@@ -2,14 +2,14 @@
 /** Collection of custom options for element modification.
  * 
  *  Please prefer `ElementOption<Element>` unless you know what you are doing. */
-export type ElementSimplifiedOptions<Element extends HTMLElement> = {
+export type ElementCustomOptions<Element extends HTMLElement> = {
     attributes: { [ attribute: string ]: string };
-    children: HTMLElement[];
+    children: HTMLElement[] | HTMLElement;
     classes: string[];
-    events: Partial<{ [ Event in keyof HTMLElementEventMap ]: (
-        this: Element,
-        event: HTMLElementEventMap[Event][] | HTMLElementEventMap[Event]
-    ) => any }>;
+    events: Partial<{ [ Event in keyof HTMLElementEventMap ]:
+            ElementEventListener<Element, Event>[] |
+            ElementEventListener<Element, Event>
+    }>;
     href: string;
     html: string;
     id: string;
@@ -18,36 +18,154 @@ export type ElementSimplifiedOptions<Element extends HTMLElement> = {
     style: Partial<CSSStyleDeclaration>;
     text: string;
 };
+
+/** Event listener callback. */
+export type ElementEventListener<
+    Element extends HTMLElement,
+    EventName extends keyof HTMLElementEventMap
+> = (
+    this: Element,
+    event: HTMLElementEventMap[EventName]
+) => any;
+
+/** Element modifier. */
+export type ElementModifier<
+    Element extends HTMLElement,
+    OptionName extends keyof ElementOptions<Element>
+> = (
+    element: Element,
+    option: ElementOptions<Element>[OptionName]
+) => void;
+
+/** Collection of element modifiers. */
+export type ElementModifiers<Element extends HTMLElement> = {
+    [ OptionName in keyof ElementOptions<Element> ]: ElementModifier<Element, OptionName>
+};
+
 /** Collection of native options for element modification.
  * 
  *  Please prefer `ElementOption<Element>` unless you know what you are doing. */
-export type ElementNativeOptions<Element extends HTMLElement> = { [ Key in keyof Element ]: Element[Key] };
+export type ElementNativeOptions<Element extends HTMLElement> = {
+    [ Key in keyof Element ]: Element[Key]
+};
+
 /** Collection of options with custom shortcuts for element modification. */
 export type ElementOptions<Element extends HTMLElement> =
-    ElementSimplifiedOptions<Element> &
-    Omit<ElementNativeOptions<Element>, keyof ElementSimplifiedOptions<Element>>;
-export type ElementModifiers<Element extends HTMLElement> = { [ Option in keyof ElementOptions<Element> ]: (
-    element: Element,
-    option: ElementOptions<Element>[Option]
-) => void };
+    ElementCustomOptions<Element> &
+    Omit<ElementNativeOptions<Element>, keyof ElementCustomOptions<Element>>;
 
 // Defines constants
 /** Default collection of modifiers. */
-export const elementDefaultModifiers: Partial<ElementModifiers<HTMLElement>> = {
+export const elementDefaultModifiers = {
     attributes: appendAttributes,
     children: appendChildren,
     classes: appendClasses,
     events: appendEvents,
-    href: modifyHref,
-    html: modifyHtml,
-    id: modifyId,
-    parent: modifyParent,
-    src: modifySrc,
+    href: replaceHref,
+    html: replaceHtml,
+    id: replaceId,
+    parent: replaceParent,
+    src: replaceSrc,
     style: appendStyle,
-    text: modifyText
-};
+    text: replaceText
+} as const;
 
 // Defines functions
+/** Appends attributes to an element's attribute list. */
+export function appendAttributes<Element extends HTMLElement>(
+    element: Element,
+    attributes: ElementOptions<Element>["attributes"]
+): void {
+    // Appends attributes
+    const attributeNames = Object.getOwnPropertyNames(attributes);
+    for(let i = 0; i < attributeNames.length; i++) {
+        const attributeName = attributeNames[i] as keyof typeof attributes & string;
+        const attribute = attributes[attributeName];
+        element.setAttribute(attributeName, attribute);
+    }
+}
+
+/** Appends children to an element's children. */
+export function appendChildren<Element extends HTMLElement>(
+    element: Element,
+    children: ElementOptions<Element>["children"]
+): void {
+    // Appends children
+    const list = Array.isArray(children) ? children : [ children ];
+    for(let i = 0; i < list.length; i++) {
+        const child = list[i];
+        element.appendChild(child);
+    }
+}
+
+/** Appends classes to an element's class list. */
+export function appendClasses<Element extends HTMLElement>(
+    element: Element,
+    classes: ElementOptions<Element>["classes"]
+): void {
+    // Appends classes
+    for(let i = 0; i < classes.length; i++) element.classList.add(classes[i]);
+}
+
+/** Appends events to an element's events. */
+export function appendEvents<Element extends HTMLElement>(
+    element: Element,
+    events: ElementOptions<Element>["events"]
+): void {
+    // Appends events
+    const eventNames = Object.getOwnPropertyNames(events);
+    for(let i = 0; i < eventNames.length; i++) {
+        const eventName = eventNames[i] as keyof typeof events & string;
+        const event = events[eventName];
+        const listeners = Array.isArray(event) ? event : [ event ];
+        for(let j = 0; j < listeners.length; j++) element.addEventListener(eventName, listeners[j]);
+    }
+}
+
+/** Appends stlye to an element's style. */
+export function appendStyle<Element extends HTMLElement>(
+    element: Element,
+    style: ElementOptions<Element>["style"]
+): void {
+    // Appensd style
+    Object.assign(element.style, style);
+}
+
+/** Clears an element's attribute list. */
+export function clearAttributes<Element extends HTMLElement>(
+    element: Element
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Clears an element's children. */
+export function clearChildren<Element extends HTMLElement>(
+    element: Element
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Clears an element's class list. */
+export function clearClasses<Element extends HTMLElement>(
+    element: Element
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Clears an element's events. */
+export function clearEvents<Element extends HTMLElement>(
+    element: Element
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Clears an element's estyle. */
+export function clearStyle<Element extends HTMLElement>(
+    element: Element
+): never {
+    throw new Error("Not implemented.");
+}
+
 /** Creates, initializes, and returns an element with a set of options. */
 export function create<TagName extends keyof HTMLElementTagNameMap>(
     tagName: TagName,
@@ -65,13 +183,53 @@ export function create<TagName extends keyof HTMLElementTagNameMap>(
     return element;
 }
 
-/** Modifies an element with a set of options. */
+/** Deletes attributes from an element's attribute list. */
+export function deleteAttributes<Element extends HTMLElement>(
+    element: Element,
+    attributes: ElementOptions<Element>["attributes"]
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Deletes children from an element's children. */
+export function deleteChildren<Element extends HTMLElement>(
+    element: Element,
+    children: ElementOptions<Element>["children"]
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Deletes classes from an element's class list. */
+export function deleteClasses<Element extends HTMLElement>(
+    element: Element,
+    classes: ElementOptions<Element>["classes"]
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Deletes events from an element's events. */
+export function deleteEvents<Element extends HTMLElement>(
+    element: Element,
+    events: ElementOptions<Element>["events"]
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Deletes style from an element's styles. */
+export function deleteStyle<Element extends HTMLElement>(
+    element: Element,
+    style: ElementOptions<Element>["style"]
+): never {
+    throw new Error("Not implemented.");
+}
+
+/** Replaces an element with a set of options. */
 export function modify<Element extends HTMLElement>(
     element: Element,
     options: Partial<ElementOptions<Element>>,
     modifiers: Partial<ElementModifiers<Element>> = elementDefaultModifiers as typeof modifiers
 ): void {
-    // Modifies element
+    // Replaces element
     const overwrites = Object.assign({}, options);
     const overwriteNames = Object.getOwnPropertyNames(overwrites);
     for(let i = 0; i < overwriteNames.length; i++) {
@@ -80,32 +238,15 @@ export function modify<Element extends HTMLElement>(
         if(typeof overwrite === "undefined") continue;
         if(overwriteName in modifiers) {
             delete overwrites[overwriteName];
-            const modifier = modifiers[overwriteName];
+            const modifier = modifiers[overwriteName] as ElementModifier<Element, typeof overwriteName>;
             if(typeof modifier === "undefined") continue;
-            modifier(element, overwrite as any);
+            modifier(element, overwrite as ElementOptions<Element>[typeof overwriteName]);
         }
     }
     Object.assign(element, overwrites);
 }
 
-/** Appends attributes to an element's attributes list.
- * 
- *  Alias of `element.setAttribute(attribute, value)`.
- */
-export function appendAttributes<Element extends HTMLElement>(
-    element: Element,
-    attributes: ElementOptions<Element>["attributes"]
-): void {
-    // Appends attributes
-    const attributeNames = Object.getOwnPropertyNames(attributes);
-    for(let i = 0; i < attributeNames.length; i++) {
-        const attributeName = attributeNames[i] as keyof typeof attributes & string;
-        const attribute = attributes[attributeName];
-        element.setAttribute(attributeName, attribute);
-    }
-}
-
-/** Replaces an element's attributes list */
+/** Replaces an element's attribute list. */
 export function replaceAttributes<Element extends HTMLElement>(
     element: Element,
     attributes: ElementOptions<Element>["attributes"]
@@ -116,42 +257,14 @@ export function replaceAttributes<Element extends HTMLElement>(
     appendAttributes(element, attributes);
 }
 
-/** Appends children to an element's children list.
- * 
- *  Alias of `element.appendChild(child)`;
-*/
-export function appendChildren<Element extends HTMLElement>(
-    element: Element,
-    children: ElementOptions<Element>["children"]
-): void {
-    // Appends children
-    for(let i = 0; i < children.length; i++) {
-        const child = children[i];
-        element.appendChild(child);
-    }
-}
-
-/** Replaces an element's children list.
- *  
- *  Alias of `element.replaceChildren(...children)`. */
+/** Replaces an element's children. */
 export function replaceChildren<Element extends HTMLElement>(
     element: Element,
     children: ElementOptions<Element>["children"]
 ): void {
     // Replaces children
-    element.replaceChildren(...children);
-}
-
-/** Appends classes to an element's class list.
- * 
- *  Alias of `element.classList.add(class)`.
-*/
-export function appendClasses<Element extends HTMLElement>(
-    element: Element,
-    classes: ElementOptions<Element>["classes"]
-): void {
-    // Appends classes
-    for(let i = 0; i < classes.length; i++) element.classList.add(classes[i]);
+    const list = Array.isArray(children) ? children : [ children ];
+    element.replaceChildren(...list);
 }
 
 /** Replaces an element's class list. */
@@ -162,24 +275,6 @@ export function replaceClasses<Element extends HTMLElement>(
     // Replaces classes
     element.classList = "";
     for(let i = 0; i < classes.length; i++) element.classList.add(classes[i]);
-}
-
-/** Appends events to an element's events.
- * 
- *  Alias of `element.addEventListener(event, listener)`.
-*/
-export function appendEvents<Element extends HTMLElement>(
-    element: Element,
-    events: ElementOptions<Element>["events"]
-): void {
-    // Appends events
-    const eventNames = Object.getOwnPropertyNames(events);
-    for(let i = 0; i < eventNames.length; i++) {
-        const eventName = eventNames[i] as keyof typeof events & string;
-        const event = events[eventName];
-        const listeners = Array.isArray(event) ? event : [ event ];
-        for(let j = 0; j < listeners.length; j++) element.addEventListener(eventName, listeners[j]);
-    }
 }
 
 /** Replaces an element's events. */
@@ -193,71 +288,50 @@ export function replaceEvents<Element extends HTMLElement>(
     appendEvents(element, events);
 }
 
-/** Modifies an element's href.
- * 
- *  Alias of `element.href = href`. */
-export function modifyHref<Element extends HTMLElement>(
+/** Replaces an element's href. */
+export function replaceHref<Element extends HTMLElement>(
     element: Element,
     href: ElementOptions<Element>["href"]
 ): void {
-    // Modifies href
+    // Replaces href
     element.setAttribute("href", href);
 }
 
-/** Modifies an element's inner html.
- * 
- *  Alias of `element.innerHTML = html`. */
-export function modifyHtml<Element extends HTMLElement>(
+/** Replaces an element's inner html. */
+export function replaceHtml<Element extends HTMLElement>(
     element: Element,
     html: ElementOptions<Element>["html"]
 ): void {
-    // Modifies html
+    // Replaces html
     element.innerHTML = html;
 }
 
-/** Modifies an element's id.
- * 
- *  Alias of `element.id = id`. */
-export function modifyId<Element extends HTMLElement>(
+/** Replaces an element's id. */
+export function replaceId<Element extends HTMLElement>(
     element: Element,
     id: ElementOptions<Element>["id"]
 ): void {
-    // Modifies id
+    // Replaces id
     element.id = id;
 }
 
-/** Modifies an element's parent.
- * 
- *  Alias of `parent.appendChild(element)` or `parent.removeChild(element)` if parent is null. */
-export function modifyParent<Element extends HTMLElement>(
+/** Replaces an element's parent. */
+export function replaceParent<Element extends HTMLElement>(
     element: Element,
     parent: ElementOptions<Element>["parent"]
 ): void {
-    // Modifies parent
-    if(parent === null) {
-        if(element.parentElement !== null) element.parentElement.removeChild(element);
-    }
-    else parent.appendChild(element);
+    // Replaces parent
+    if(parent !== null) parent.appendChild(element);
+    else if(element.parentElement !== null) element.parentElement.removeChild(element);
 }
 
-/** Modifies an element's src.
- * 
- *  Alias of `parent.src = src;`. */
-export function modifySrc<Element extends HTMLElement>(
+/** Replaces an element's src. */
+export function replaceSrc<Element extends HTMLElement>(
     element: Element,
     src: ElementOptions<Element>["src"]
 ): void {
-    // Modifies src
+    // Replaces src
     element.setAttribute("src", src);
-}
-
-/** Appends stlye to an element's style. */
-export function appendStyle<Element extends HTMLElement>(
-    element: Element,
-    style: ElementOptions<Element>["style"]
-): void {
-    // Appensd style
-    Object.assign(element.style, style);
 }
 
 /** Replaces an element's style. */
@@ -270,13 +344,11 @@ export function replaceStyle<Element extends HTMLElement>(
     appendStyle(element, style);
 }
 
-/** Modifies an element's inner text.
- * 
- *  Alias of `element.innerText = text`. */
-export function modifyText<Element extends HTMLElement>(
+/** Replaces an element's inner text. */
+export function replaceText<Element extends HTMLElement>(
     element: Element,
     text: ElementOptions<Element>["text"]
 ): void {
-    // Modifies text
+    // Replaces text
     element.innerText = text;
 }
--- web/zero.ts
+++ web/zero.ts
@@ -1,16 +1,16 @@
 // Defines private constants
 /** Alias for `document`. */
 const page = document;
+
 /** Alias for `Object.defineProperty`. */
 const put = Object.defineProperty;
-/** Alias for `Symbol.for("z")`. */
-const sign = Symbol.for("z");
-/** Alias for `querySelect`. */
-const select = page.querySelectorAll.bind(page);
+
+/** Alias for `"querySelector"`. */
+let querySelector = "querySelector";
 
 // Defines private functions
 /** Deep assigns object. */
-const assign = (target: any, data: object): void => {
+const assign = (target: any, data: Record<string, any>): void => {
     for(const key in data) {
         const value = data[key as keyof typeof data];
         typeof value === "object" ? assign(target[key], value) : target[key] = value;
@@ -20,17 +20,15 @@ const assign = (target: any, data: object): void => {
 /** Wraps then iterates element with custom shortcuts. */
 const modify = (element: HTMLElement, properties: object): HTMLElement => {
     // Wraps element
-    if(!(sign in element)) {
-        const define = (property: string, map: string) => put(element, property, {
-            // @ts-ignore
-            get: () => element[map], set: (value: any) => element[map] = value
-        });
-        define("text", "innerText");
-        define("html", "innerHTML");
-        define("class", "classList");
+    const define = (property: string, map: string) => !(0 in element) && put(element, property, {
         // @ts-ignore
-        element[sign] = 0;
-    }
+        get: () => element[map], set: (value: any) => element[map] = value
+    });
+    define("text", "innerText");
+    define("html", "innerHTML");
+    define("class", "classList");
+    // @ts-ignore
+    element[0] = 0;
 
     // Iterates properties
     assign(element, properties);
@@ -40,18 +38,16 @@ const modify = (element: HTMLElement, properties: object): HTMLElement => {
 }
 
 // Defines functions
-/** Query selects and assigns properties to element.
- * 
- *  `z(query, properties)` stands for `zero(element, properties)`. */
-export const z = (query: string, properties: object): HTMLElement => modify(select(query)[0] as HTMLElement, properties);
+/** Query selects and assigns properties to element. */
+export const z = (query: string, properties: object): HTMLElement =>
+    // @ts-ignore
+    modify(page[querySelector](query) as HTMLElement, properties);
 
-/** Query selects assigns properties to multiple elements.
- * 
- *  `zs(query, properties)` stands for `zeros(element, properties)`. */
+/** Query selects assigns properties to multiple elements. */
 export const zs = (query: string, properties: object): HTMLElement[] => {
     // Iterates elements
     // @ts-ignore
-    const elements = [ ...select(query) ]
+    const elements = [ ...page[querySelector + "All"](query) ]
         .map(element => modify(element as HTMLElement, properties)) as HTMLElement[];
 
     // Defines shortcuts
