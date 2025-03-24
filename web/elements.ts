27590bdb91daf778067584980ae010c66c6b9b07
DmmD GM <Dm12332131mDGaming@gmail.com>
2025-03-24 12:55:26 +0000

    save

--- web/elements.ts
+++ web/elements.ts
@@ -1,3 +1,5 @@
+import { create, modify } from "./element";
+
 export type ElementShortcuts<TargetElement extends HTMLElement> = {
     attributes: { [ attribute: string ]: string };
     children: HTMLElement[] | HTMLElement;
@@ -18,3 +20,27 @@ export type ElementShortcuts<TargetElement extends HTMLElement> = {
 export type ElementProperties<TargetElement extends HTMLElement> = {
     [ Key in keyof TargetElement ]: TargetElement[Key]
 };
+
+export type ElementListener<
+    TargetElement extends HTMLElement,
+    EventName extends keyof HTMLElementEventMap
+> = (this: TargetElement, event: HTMLElementEventMap[EventName]) => any;
+
+export type ElementOptions<TargetElement extends HTMLElement> =
+    ElementShortcuts<TargetElement> &
+    Omit<ElementProperties<TargetElement>, keyof ElementShortcuts<TargetElement>>;
+
+export function create<TagName extends keyof HTMLElementTagNameMap>(
+    tagName: TagName,
+    options: Partial<ElementOptions<HTMLElementTagNameMap[TagName]>>
+): HTMLElementTagNameMap[TagName] {
+    const targetElement = document.createElement(tagName);
+    return modify(targetElement, options);
+}
+
+export function modify<TargetElement extends HTMLElement>(
+    targetElement: TargetElement,
+    options: Partial<ElementOptions<TargetElement>>
+): TargetElement {
+    
+}
