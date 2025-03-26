bce534097efc024fd4927b0e07a11a6fa725b7ee
DmmD GM <Dm12332131mDGaming@gmail.com>
2025-03-26 14:46:50 +0000

    save

--- docs/css.md
+++ docs/css.md
@@ -11,7 +11,7 @@ A collection of styles in `.css` files for web development.
 Removes all margins and paddings on `html`, `body`, and `#app`, and forces them to always stretch to the page's maximum
 height and width.
 
-### Example
+### Usage
 ```html
 <link href="%dmmd-ts%/css/reset-body.css" rel="stylesheet">
 ```
--- type/arrayable.d.ts
+++ type/arrayable.d.ts
@@ -2,11 +2,11 @@
 /** Converts a type into both its single form and its array form. */
 export type Arrayable<Element extends any> = Element[] | Element;
 
-/** Converts a type into both its array form and its single form. */
-export type Singleable<Element extends any[]> = Element | Element[number];
-
 /** Forces an arrayable type to collapse into its single form. */
 export type NonArrayable<Element extends any> = Element extends any[] ? Element[number] : Element;
 
 /** Forces a singleable type to collapse into its array form. */
 export type NonSingleable<Element extends any> = Element extends any[] ? Element : Element[];
+
+/** Converts a type into both its array form and its single form. */
+export type Singleable<Element extends any[]> = Element | Element[number];
