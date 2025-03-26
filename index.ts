b6cee26b7a5ec27a4cde6f57fccd0613245e56b9
DmmD GM <dm12332131mdgaming@gmail.com>
2025-03-26 17:48:21 -0400

    forget about the docs

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 // Exports
+export type * as type from "./type";
 export * as vanilla from "./vanilla";
 export * as web from "./web";
--- vanilla/queue.ts
+++ vanilla/queue.ts
@@ -1,32 +1,32 @@
-// Defines classes
-export class Queue<Element extends unknown> {
-    private _array: Element[] | null;
-    private _set: Set<Element>;
+// // Defines classes
+// export class Queue<Element extends unknown> {
+//     private _array: Element[] | null;
+//     private _set: Set<Element>;
 
-    constructor() {
-        this._array = null;
-        this._set = new Set();
-    }
+//     constructor() {
+//         this._array = null;
+//         this._set = new Set();
+//     }
 
-    clear(): void {
+//     clear(): void {
         
-    }
+//     }
 
-    delete(element: Element): void {
+//     delete(element: Element): void {
 
-    }
+//     }
 
-    pull(): Element | null {
-        return null;
-    }
+//     pull(): Element | null {
+//         return null;
+//     }
 
-    push(bump: boolean = false): void {
+//     push(bump: boolean = false): void {
         
-    }
+//     }
 
-    test(element: Element): boolean {
-        return false;
-    }
+//     test(element: Element): boolean {
+//         return false;
+//     }
 
-    toSet()
-}
+//     toSet()
+// }
