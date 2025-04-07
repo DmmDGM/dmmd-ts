28d83bf19d5606e9b1aa76ffe37d163ec73225f6
DmmD GM <dm12332131mdgaming@gmail.com>
2025-04-07 00:49:53 -0400

    ?

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,5 @@
 // Exports
-declare module "dmmd-ts/type" {
-    export type * as type from "./type";
-};
-declare module "dmmd-ts/bun" {
-    export * as bun from "./bun";
-};
-declare module "dmmd-ts/util" {
-    export * as util from "./util";
-};
-declare module "dmmd-ts/web" {
-    export * as web from "./web";
-};
+declare module "dmmd-ts/type/*" {};
+declare module "dmmd-ts/bun/*" {};
+declare module "dmmd-ts/util/*" {};
 declare module "dmmd-ts/web/*" {};
