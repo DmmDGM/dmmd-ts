51f3218e0072a6a7f4c63a3253bc9c9c13509865
DmmD GM <Dm12332131mDGaming@gmail.com>
2025-04-09 15:16:45 +0000

    split pcall into its async and sync

--- util/pcall.ts
+++ util/pcall.ts
@@ -1,6 +1,22 @@
 // Defines functions
-/** Runs callback and returns the result or a fallback value if an error occurs. */
-export function attempt<Value extends any>(
+/** Runs an asynchronous callback and returns the result or a fallback value if an error occurs. */
+export async function attemptAsync<Value extends any>(
+    callback: () => Promise<Value>,
+    fallback: Promise<Value> | Value
+): Promise<Value> {
+    // Attempts callback
+    try {
+        return await callback();
+    }
+    
+    // Returns fallback
+    catch {
+        return await fallback;
+    }
+}
+
+/** Runs a synchronous callback and returns the result or a fallback value if an error occurs. */
+export function attemptSync<Value extends any>(
     callback: () => Value,
     fallback: Value
 ): Value {
@@ -15,8 +31,23 @@ export function attempt<Value extends any>(
     }
 }
 
-/** Runs callback and returns the result and the error if one occurs. */
-export function inspect<Value extends any>(
+/** Runs an asynchronous callback and returns the result and the error if one occurs. */
+export async function runAsync<Value extends any>(
+    callback: () => Promise<Value>
+): Promise<[ Value, null, null ] | [ null, Error, unknown ]> {
+    // Attempts callback
+    try {
+        return [ await callback(), null, null ];
+    }
+
+    // Returns error
+    catch(error) {
+        return [ null, error instanceof Error ? error : new Error(String(error)), error ];
+    }
+}
+
+/** Runs a synchronous callback and returns the result and the error if one occurs. */
+export function runSync<Value extends any>(
     callback: () => Value
 ): [ Value, null, null ] | [ null, Error, unknown ] {
     // Attempts callback
